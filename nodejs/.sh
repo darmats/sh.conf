fcbc5890fbf64ebdd9007883fe70a3128bb66e9c
Hiroki Matsuda <h.matsu0330@gmail.com>
2018-10-27 01:33:02 +0900

    Disable nodebrew

--- nodejs/.sh
+++ nodejs/.sh
@@ -1,1 +1,4 @@
-export PATH=$HOME/.nodebrew/current/bin:$PATH
+# nodebrew
+# if builtin command -v ${HOME}/.nodebrew/current/bin/nodebrew > /dev/null; then
+#   export PATH=$HOME/.nodebrew/current/bin:$PATH
+# fi
