9db0905a1f80e216c5ca148da8fefb88b5007355
Hiroki Matsuda <h.matsu0330@gmail.com>
2018-10-28 23:55:04 +0900

    wip

--- git/.bash
+++ git/.bash
@@ -10,3 +10,12 @@
 
 # # show directory only -> show path \W -> \w
 # export PS1='\[\033[34m\]\h\[\033[0m\]:\u \[\033[33m\]\W\[\033[0m\] \[\033[31m\]$(__git_ps1 "[%s]")\[\033[0m\]\$ '
+
+# https://github.com/magicmonty/bash-git-prompt
+# brew install bash-git-prompt
+if [ ! -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+  return
+fi
+
+__GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
