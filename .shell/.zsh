2ece3a6bd250ea23371b3dab4e40e2d84cef7d42
Hiroki Matsuda <h.matsu0330@gmail.com>
2018-11-27 15:56:14 +0900

    Add zsh peco alias

--- .shell/.zsh
+++ .shell/.zsh
@@ -2,4 +2,7 @@ PROMPT=$'%m:%F{cyan}%n%f %F{yellow}%c%f %% '
 
 alias -g @g=' | grep'
 alias -g @gi=' | grep -i'
+if [ -n $(command -v peco) ]; then
+    alias -g @p=' | peco'
+fi
 alias -g @x=' | xargs'
