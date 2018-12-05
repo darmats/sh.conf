446b7ab0e380ad970d8c260478d6d39501a46587
Hiroki Matsuda <h.matsu0330@gmail.com>
2018-12-05 11:49:27 +0900

    Update

--- .shell/.zsh
+++ .shell/.zsh
@@ -1,9 +1,6 @@
-# PROMPT=$'%m:%F{cyan}%n%f %F{yellow}%c%f %% '
-PROMPT=$'%{$fg_bold[green]%}%n@%m%{\e[0m%}:%{$fg_bold[blue]%}%~%{\e[0m%}$ '
-
 alias -g @g=' | grep'
 alias -g @gi=' | grep -i'
 if [ -n $(command -v peco) ]; then
-    alias -g @p=' | peco'
+  alias -g @p=' | peco'
 fi
 alias -g @x=' | xargs'
