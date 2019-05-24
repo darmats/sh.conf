e1b6f14574eff2837c7796ece0db415d25994bbe
Hiroki Matsuda <h.matsu0330@gmail.com>
2019-05-24 15:03:48 +0900

    Update composer path

--- 01.php/.sh
+++ 01.php/.sh
@@ -1,3 +1,5 @@
 if [ -d ${HOME}/.composer/vendor/bin ] ; then
   export PATH=$PATH:${HOME}/.composer/vendor/bin
+elif [ -d ${HOME}/.config/composer/vendor/bin ] ; then
+  export PATH=$PATH:${HOME}/.config/composer/vendor/bin
 fi
