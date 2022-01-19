4639f260c9d935f90e2f863fa7689c73bb142e5e
Hiroki Matsuda <h.matsu0330@gmail.com>
2022-01-19 15:26:30 +0900

    Update "cdsrc" command, and add "cdsrca" command.

--- ghq/.sh
+++ ghq/.sh
@@ -3,5 +3,13 @@ if [ -z $GHQ_ROOT ]; then
 fi
 
 if [ -n $(command -v fzf) ]; then
-  alias cdsrc='cd ${GHQ_ROOT}/$(ghq list | fzf)'
+  function cdsrc() {
+    if [ $# -eq 0 ]; then
+      echo "Usage: cdsrc [pattern]"
+      return 1
+    else
+      cd ${GHQ_ROOT}/$(ghq list | grep $1 | fzf)
+    fi
+  }
+  alias cdsrca='cd ${GHQ_ROOT}/$(ghq list | fzf)'
 fi
