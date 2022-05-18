ce87fbfeb0c529c91c0bce9d83379da977c52410
Hiroki Matsuda <h.matsu0330@gmail.com>
2022-05-19 03:55:46 +0900

    fix

--- asdf/.zsh
+++ asdf/.zsh
@@ -1,11 +1,11 @@
-ASDF_HOME="${CNF_ASDF_HOME:/opt/homebrew/opt/asdf}"
+ASDF_HOME="${CNF_ASDF_HOME:-${HOMEBREW_PREFIX}/opt/asdf}"
 
-if [ ! -f ${ZINIT_HOME}/libexec/asdf.sh ]; then
+if [ ! -f ${ASDF_HOME}/libexec/asdf.sh ]; then
   return
 fi
 if [ ! -f "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc" ]; then
   return
 fi
 
-. ${ZINIT_HOME}/libexec/asdf.sh
+. ${ASDF_HOME}/libexec/asdf.sh
 source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
