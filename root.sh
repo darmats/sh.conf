01b922552d05b73973394a21f5a8a0784b5aac67
Hiroki Matsuda <h.matsu0330@gmail.com>
2022-08-10 23:38:51 +0900

    Set homebrew prefix

--- root.sh
+++ root.sh
@@ -2,6 +2,12 @@ if [ -f ${CNF_ROOT}/secretrc.sh ] ; then
   source ${CNF_ROOT}/secretrc.sh
 fi
 
+if [ -z ${HOMEBREW_PREFIX} ]; then
+  if [ -n $(command -v brew) ]; then
+    HOMEBREW_PREFIX="$(brew --prefix)"
+  fi
+fi
+
 source $CNF_ROOT/list.sh
 for CNF_SH in $CNF_LIST; do
   f=${CNF_ROOT}/${CNF_SH}/.sh
